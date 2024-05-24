e5150cf6d460bd81f24cf6fbdd1013417774a36c
Harrow <This@me.de>
2024-05-24 12:52:13 +0200

    changed pytesseract to easyocr

--- mainv1.py
+++ mainv1.py
@@ -4,30 +4,32 @@
 import json
 import argparse
 import os
-import pytesseract
+#import pytesseract
 import csv
 import ast
-#from imutils import contours
-#import imutils
 import time
 import cv2
+import easyocr
 import numpy as np
-#from pynput.mouse import Listener
+
 from PIL import Image
-#from number_recognition import NumberRecognizer
+
 import itertools
 from multiprocessing import Pool,Value,Process,Lock
+reader = None
 ## Shadystuff for global mouseAction event
 x,y,h,w = 0,0,100,100
 frame,clone = None,None
 worker_progress, RANGE = 0,0
-rectangles = [(893, 397, 54, 54), (1213, 388, 78, 78)]
-
+rectangles = [(963, 494, 84, 84)]
+threshold_arg = None
 DEST = "Screenshots\\"
-pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Niklas\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+#pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Niklas\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+write_screenshot_arg = False
+csv_out_path_arg = "output.csv" 
 ##! Not used
 
-def get_screenshots_rect(interval):
+def get_screenshots_rect(interval: int) -> None:
     """Saves a 28x28 screenshot every interval seconds"""
     global clone
     assert type(interval) == int
@@ -63,7 +65,7 @@ def get_screenshots_rect(interval):
 ##* Ab hier gehts los
 
 
-def  mouseAction(event,x_loc,y_loc,flags,param):
+def  mouseAction(event: int,x_loc: float,y_loc: float,flags: int,param) -> None:
     global x,y,h,w, frame,i, clone
     if event == 1:  # Left Click
         x = x_loc - w//2
@@ -89,15 +91,15 @@ def  mouseAction(event,x_loc,y_loc,flags,param):
     cv2.imshow('Frame', frame)
 
 
-def save_screenshots(path:str,interval:int=10,fps=29.97,start:int=26,dil=(1,1),threshold=130,end=-1):
+def save_screenshots(path:str,interval:int=10,fps=29.97,start:int=0,dil=(1,1),threshold=130,end=-1):
     """Saves a screenshot from pathvideo every interval seconds
        Frame000i.png 
        -> returns array of array with parsed numbers
     """
-    global rectangles
+    global rectangles,threshold_arg, write_screenshot_arg
     numbers = []
     if not interval: interval = 10
-    if not start: start = 26
+    if not start: start = 0
     if not fps: fps = 29.97
     assert type(interval) == int
 
@@ -106,6 +108,13 @@ def save_screenshots(path:str,interval:int=10,fps=29.97,start:int=26,dil=(1,1),t
         print("File did not load")
         raise SystemExit
     i = 0
+    if threshold_arg: threshold = threshold_arg
+    print(f'[*] Reading... ')
+    print(f" {'Interval':16}: {interval}")
+    print(f" {'Fps':16}: {fps}")
+    print(f" {'threshold':16}: {threshold}")
+    print(f" {'dilatation':16}: {dil}")
+    print(f" {'Make Screenshots':16}: {write_screenshot_arg}")
 
     while (cap.isOpened()):
         pos = int(round(i * fps * interval + (fps * start)))
@@ -136,6 +145,7 @@ def transform_frame(img,dil,threshold):
     img = cv2.bitwise_not(img)
     #cv2.imwrite("test.png",img)
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
 def rotate_image(image, angle):
   image_center = tuple(np.array(image.shape[1::-1]) / 2)
   rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
@@ -172,7 +182,7 @@ def make_screen_right_again_noisy(img,threshhold,dil):
 def choose_area(path):
     """Loads Video and select n areas"""
     global clone,rectangles
-
+    rectangles = []
     cap = cv2.VideoCapture(path)
     if (not cap.isOpened()):
         print("File did not load")
@@ -204,19 +214,29 @@ def choose_area(path):
 
 def split_frame_read_number(rectangles,frame,i,dil,threshold):
     """Takes array of rectangles (x,y,w,h) and cuts them out frame i
-        -> return number from all frames as array of strings
+        -> return ocr read number from all frames as array of strings
     """
-    global DEST
+    global DEST,write_screenshot_arg
     out = []
     j = 0
-   
+    ##print("FIXED RECTANGLES")
+    ##rectangles = [(963, 494, 84, 84)]
     for (x,y,w,h) in rectangles:
         j+=1
-        if j == 2:
-            frame = rotate_image(frame,3)
-        #cv2.imwrite(f'{DEST}Frame{i:04d}_{j}.png',transform_frame(frame[y:y+h,x:x+w],dil,threshold))
-        num = get_numbers(transform_frame(frame[y:y+h,x:x+w],dil,threshold)).replace("\n","")
-        out.append(num)
+        # if j == 2: ##! kinda weird
+        #     frame = rotate_image(frame,3)
+        if write_screenshot_arg: cv2.imwrite(f'{DEST}Frame{i:04d}_{j}.png',transform_frame(frame[y:y+h,x:x+w],dil,threshold))
+        # cv2.imshow(transform_frame(frame[y:y+h,x:x+w],dil,threshold))
+        
+        tmp = transform_frame(frame[y:y+h,x:x+w],dil,threshold)
+        if not np.any(tmp):
+            print("Frame consists only of zeros (Black)")
+            num = ["Blackscreen"]
+        else:
+            num = get_numbers_ocr(transform_frame(frame[y:y+h,x:x+w],dil,threshold))
+        if num == []: num = ["N/A"]  #get_numbers_ocr can return None if only 0 are given
+        print("num gets appended:",num[0], type(num))
+        out.append(num[0])
         # if not num:
         #     print(f'Frame{i:04d}_{j}.png',num,"<--------------------")
             
@@ -226,26 +246,42 @@ def split_frame_read_number(rectangles,frame,i,dil,threshold):
 
 def init_argparse():
     """Parse args; make Folder for Screenshots"""
-    global DEST
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-video',type=str,help="Path to the file")
+    global DEST, threshold_arg, write_screenshot_arg,csv_out_path_arg
+    parser = argparse.ArgumentParser(description="Select areas from the video which get OCRed every *interval* seconds", 
+                                     epilog="""1. Select the areas via mouse. Use mousewheel to change size of the rectangle.\n\t 
+                                     Use leftclick to set a rectangle. \n\t 
+                                     Use q to quit once you finished.
+                                     2. 
+                                     """)
+    parser.add_argument('-video',required=True,type=str,help="Path to the file")
     parser.add_argument('-fps',type=int,help="set fps value [default 30]")
     parser.add_argument('-interval',type=int,help="set interval in seconds [default 10]")
-    parser.add_argument('-start',type=int,help="timestamp for the first screenshot in seconds [default 26]")
-    parser.add_argument('-dest',type=str,help ="Path to destination folder")
-    
+    parser.add_argument('-start',type=int,help="timestamp for the first screenshot in seconds [default 0]")
+    parser.add_argument('-dest',type=str,help ="Path to destination folder (for Screenshots)")
+    parser.add_argument('-tesseract_path',type=str,help="Path to pytesseract.exe")
+    parser.add_argument('-debug','-d',action="store_true")
+    parser.add_argument('-threshold', help="Value for the img conversion (0-255) [default 130]")
+    parser.add_argument('-screenshot',help="Write Screenshots to dest or Screenshots/ [default False]", action="store_true",default=False)
+    parser.add_argument('-out',type=str,default="output.csv", help="Path/name to the output csv [default output.csv]")
     args = parser.parse_args()
+    threshold_arg = args.threshold
+    csv_out_path_arg = args.out
+    write_screenshot_arg = args.screenshot
+    # pytesseract.pytesseract.tesseract_cmd = args.tesseract_path
     if args.dest: 
         os.makedirs(args.dest,exist_ok=True)
         DEST = args.dest+"\\"
     else: 
         #os.rmdir("Screenshots")
         os.makedirs("Screenshots",exist_ok=True)
-    return args.video, args.fps,args.interval,args.start
+    return args.video, args.fps,args.interval,args.start,args.debug
 
-def get_numbers(img):
+def get_numbers_ocr(img):
     """reads digits from a picture via teseract"""
-    return pytesseract.image_to_string(img, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+    global reader
+    return reader.readtext(img, detail=0)
+    # return pytesseract.image_to_string(img, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+    
 
 def read_values_from_csv(path:str):
     """reads values from csv-file evaluated by hand
@@ -259,8 +295,38 @@ def read_values_from_csv(path:str):
             out.append([row[1].replace(",","."),row[2]])
     out = out[1:] # erste Zeile skippen
     return out
-            
-            
+
+def numbers_to_csv(arr:list[list[str]]) -> None:
+    """Takes list with lists of numbers and writes them to dest_path (-out param)"""
+    global csv_out_path_arg
+    dest_path = csv_out_path_arg
+    try: 
+        csvfile = open(dest_path, 'w', newline='')
+    except:
+        print("[ ! ] Can't create csv-file!")
+        return
+    print("RECTANGLES",rectangles)
+    writer = csv.writer(csvfile,dialect='excel')
+    column_names = None
+    while True:
+        column_names = input(f"Write {len(rectangles)} column names as you got rectangles [seperated with ,]:")
+        if len(column_names.split(',')) == len(rectangles):
+            break
+        print("invalid number of names")
+        print("Should be", len(rectangles))
+    if column_names:
+        writer.writerow(column_names.split(','))
+    else:
+        writer.writerow(["Column"+str(j) for j in range(arr[0])])
+
+    for row in arr:
+        writer.writerow(row)
+    
+    
+    csvfile.close()
+    print("[+] results in", dest_path)
+    return
+
 def worker(param):
     """
     returns dict(threshold, (#mistakes0, #mistakes1), [wrong values (index frame, 0 or 1,wrong value, right value)])
@@ -270,7 +336,7 @@ def worker(param):
     right_values,dil,threshold,ran = param
     print("Starting with", (dil,dil), threshold)
 
-    path, fps, interval,start = init_argparse()
+    path, fps, interval,start,_ = init_argparse()
     ocr_values = save_screenshots(path,interval,fps,start,(dil,dil),threshold)
     mistake_count = [0,0]
     wrong_values = []
@@ -295,7 +361,7 @@ def sort_1(elem):
 def test_bench(worker_threads:int,threshold_start:int, threshold_end:int,threshold_stepsize:int,dilate_start:int,dilate_end:int,path_to_csv:str):
 
     ran = ((threshold_end - threshold_start)//threshold_stepsize)*(dilate_end-dilate_start+1)
-    path, fps, interval,start = init_argparse()
+    path, fps, interval,start,_ = init_argparse()
     right_values = read_values_from_csv("Daten_IONIQ5_WLTC_Eco_handgeschrieben.csv")
     dil = [i for i in range(dilate_start,dilate_end+1)]
     thresholds = [i for i in range(threshold_start,threshold_end,threshold_stepsize)]
@@ -368,23 +434,47 @@ def print_candidates(arr,limit=10,fromFile=False):
     for t in arr[:limit]:
         print(f'{t["threshold"]: <10}{t["dilate"]: ^10}{t["mistakes"][1]: ^10}')
 def main():
-
-    path, fps, interval,start = init_argparse()
+    global reader
+    path, fps, interval,start, _ = init_argparse()
     #get_screenshots(10)
     #get_numbers()
     #transform_frame(1,1)
     t = time.time()
-    #choose_area(path)
+    # print("FIXED RECTANGLE ACTIVE, SKIPPING CHOOSE_AREA")
+    choose_area(path)
+    ## TODO implement character space: reader.readtext(input, allowlist ='0123456789')
+    reader = easyocr.Reader(['en'])
+    results = save_screenshots(path,interval,fps,start)
+    print(results)
+    numbers_to_csv(results)
     
-    save_screenshots(path,interval,fps,start)
     print(time.time()-t)
+    
 
+def debug_main() -> None:
+    """Executes Test/Debug stuff
+        Put Changes here
+    """
+    
+    
+    # test_bench(4,35,230,3,0,2,"Daten_IONIQ5_WLTC_Eco_handgeschrieben.csv")
 
-
+def test():
+    print("Calling test")
+    img = cv2.imread('Screenshots\\Frame0000_1.png')
+    print(get_numbers_ocr(img))
 if __name__ == "__main__":
+
+
+
+    if init_argparse()[4]:
+        debug_main()
+    else:
+        main()
+    
     #main()
     # worker_threads, threshold start_value, thresholdend_value,threshold stepsize, dilate start, dilate end, path to csv
-    test_bench(4,35,230,3,0,2,"Daten_IONIQ5_WLTC_Eco_handgeschrieben.csv")
+    
     #read_values_from_csv("Daten_IONIQ5_WLTC_Eco_handgeschrieben.csv")
     #print_candidates("outputNov2117_13_00.txt",10,True)
-    pass
+    
